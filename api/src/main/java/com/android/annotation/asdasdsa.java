d8f9e2fd0a600e60a826fd5ce412a888f3ed6d50
kiddot <kiddo1024@163.com>
2017-08-06 17:50:40 +0800

    delete

--- api/src/main/java/com/android/annotation/asdasdsa.java
+++
@@ -1,8 +0,0 @@
-package com.android.annotation;
-
-/**
- * Created by kiddo on 17-8-6.
- */
-
-public class asdasdsa {
-}
--- api/src/main/java/com/android/annotation/template/ISyringe.java
+++
@@ -1,9 +0,0 @@
-package com.android.annotation.template;
-
-/**
- * Created by kiddo on 17-8-6.
- */
-
-public interface ISyringe {
-    void inject(Object target);
-}
